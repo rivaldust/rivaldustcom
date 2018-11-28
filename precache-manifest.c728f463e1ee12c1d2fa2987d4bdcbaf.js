c5596b1ae205be1db28924e32e1c2d1100993a55
Chris Goodwin <chrisj.goodwin@gmail.com>
2018-11-27 20:08:44 -0800

    Updates

---
+++ precache-manifest.c728f463e1ee12c1d2fa2987d4bdcbaf.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "be3f48451a87c1059569",
+    "url": "/rivaldustcom/static/css/main.21e0c524.chunk.css"
+  },
+  {
+    "revision": "be3f48451a87c1059569",
+    "url": "/rivaldustcom/static/js/main.be3f4845.chunk.js"
+  },
+  {
+    "revision": "30c7d205af169fb1d20d",
+    "url": "/rivaldustcom/static/css/1.d9378272.chunk.css"
+  },
+  {
+    "revision": "30c7d205af169fb1d20d",
+    "url": "/rivaldustcom/static/js/1.30c7d205.chunk.js"
+  },
+  {
+    "revision": "17d9d8dc342c6faa2592",
+    "url": "/rivaldustcom/static/js/runtime~main.17d9d8dc.js"
+  },
+  {
+    "revision": "3c56f235194d09a117df96953ed43778",
+    "url": "/rivaldustcom/static/media/rival_dust_icon_header.3c56f235.png"
+  },
+  {
+    "revision": "ac2be5956828ce73231bde4c6b5ce2db",
+    "url": "/rivaldustcom/static/media/Sniper_CityScape.ac2be595.jpg"
+  },
+  {
+    "revision": "d30769bc994ab3ad9c46c60ac7f4fbf1",
+    "url": "/rivaldustcom/index.html"
+  }
+];
